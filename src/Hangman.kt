a74b1c288598bec068f549127c1b6b803d4d8c54
denis_levchenko <tranzi90@gmail.com>
2024-09-30 23:57:30 +0300

    init

---
+++ src/Hangman.kt
@@ -0,0 +1,131 @@
+import java.util.*
+import kotlin.random.Random
+
+val words = listOf("elbow", "writer", "circle", "polish", "bridge", "store", "fang", "scarecrow", "show", "jeans", "wilderness", "attempt", "waxing", "aftermath", "banana", "wrist", "wheel", "spring", "cherries", "nerve")
+var word = ""
+val guesses = arrayListOf<Char>()
+var remainingGuesses = 6
+var mistakes = 0
+
+fun main() {
+    setupGame()
+}
+
+fun setupGame(): Unit {
+    val wordIndex = Random.nextInt(words.size)
+    word = words[wordIndex].uppercase(Locale.getDefault())
+    println(word)
+
+    for (i in word.indices)
+        guesses.add('_')
+
+    var gameOver = false
+
+    do {
+        printGameStatus()
+        println("Please enter a letter:")
+        val input = readLine()?:""
+
+        if (input.isEmpty()) {
+            println("That's not a valid input. Please try again")
+        } else {
+
+        }
+
+    } while (!gameOver)
+}
+
+fun printGameStatus(): Unit {
+    when(mistakes) {
+        0 -> print0Mistakes()
+        1 -> print1Mistake()
+        2 -> print2Mistakes()
+        3 -> print3Mistakes()
+        4 -> print4Mistakes()
+        5 -> print5Mistakes()
+        6 -> print6Mistakes()
+    }
+
+    print("Word: ")
+    for (element in guesses)
+        print(element + " ")
+
+    println("\nYou have $remainingGuesses guess(es) left")
+}
+
+fun print0Mistakes() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |        ")
+    println("  |        ")
+    println("  |        ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print1Mistake() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |        ")
+    println("  |        ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print2Mistakes() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |      | ")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print3Mistakes() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /| ")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print4Mistakes() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /|\\")
+    println("  |      | ")
+    println("  |        ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print5Mistakes() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /|\\")
+    println("  |      | ")
+    println("  |     /  ")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
+
+fun print6Mistakes() {
+    println("  |------|-")
+    println("  |      | ")
+    println("  |      O ")
+    println("  |     /|\\")
+    println("  |      | ")
+    println("  |     / \\")
+    println(" /|\\      ")
+    println("/ | \\     ")
+}
